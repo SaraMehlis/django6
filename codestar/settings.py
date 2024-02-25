a0a7090e488815949fe77509aec80d859dbe44c0
Sara mehlis <Sara.mehlis@gmail.com>
2024-02-25 10:55:26 +0000

    add comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-saramehlis-django6-zv1arggii4p.ws-eu108.gitpod.io','.herokuapp.com']
 
